3e90faf4fd552c3f7dcce177ebd104ddb335a2bd
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-01-16 03:26:27 +0200

    update readme

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,18 +5,6 @@
             <div class="card">
                 <div class="card-header">Login Form</div>
                 <div class="card-body">
-                    <!--
-                        why we validate?
-                        every day there are:
-                        500 million tweets
-                        294 billion emails
-                        5 billion searches
-                        300 new posts on laracasts
-                        64.5k downloads of spatie
-                        then there is alot of data => through APIs or in forms
-                        the data pass between servers and users
-                        Can not we trust our users?
-                    -->
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
                         <div class="form-group row">
