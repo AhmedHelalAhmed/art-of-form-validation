d19afbe840f222306bb3e29f4ebcc1494488b298
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2020-01-16 03:09:32 +0200

    Add login form

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,8 +3,65 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Form</div>
-
+                <div class="card-header">Login Form</div>
+                <div class="card-body">
+                    <!--
+                        why we validate?
+                        every day there are:
+                        500 million tweets
+                        294 billion emails
+                        5 billion searches
+                        300 new posts on laracasts
+                        64.5k downloads of spatie
+                        then there is alot of data => through APIs or in forms
+                        the data pass between servers and users
+                        Can not we trust our users?
+                    -->
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label"
+                                >Email address</label
+                            >
+                            <div class="col-md-6">
+                                <input
+                                    id="email"
+                                    type="email"
+                                    class="form-control @error('email') is-invalid @enderror"
+                                    name="email"
+                                    value="{{ old('email') }}"
+                                    autocomplete="email"
+                                />
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label
+                                for="password"
+                                class="col-md-4 col-form-label"
+                                >Password</label
+                            >
+                            <div class="col-md-6">
+                                <input
+                                    id="password"
+                                    type="password"
+                                    class="form-control @error('password') is-invalid @enderror"
+                                    name="password"
+                                    autocomplete="current-password"
+                                />
+                            </div>
+                        </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    SIGN IN
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="card mt-3">
+                <div class="card-header">Register Form</div>
                 <div class="card-body">
                     <form
                         method="POST"
